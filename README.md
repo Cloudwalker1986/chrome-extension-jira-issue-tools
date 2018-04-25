516448bfd38c4d038092854ec67004b24f50b2b7
Fabrizio Gargiulo <fabrizio.gargiulo@rocket-internet.com>
2018-04-25 17:16:37 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# chrome-extension-jira-issue-tools
+# Chrome Extension: JIRA issue tools
 A Chrome extension with some tools regarding JIRA issues, e.g. branch name, first commit message, migration file name...
+
+## Setup
+* Clone the project locally
+* Open "Google Chrome" settings menu > More Tools > Extensions
+* Click on "Load unpacked" button and specify the folder of the just cloned extension
+
+## Pre requisite
+This extension only works if the current page url match the following patterns
+* https://jira.rocket-internet.de/browse/TML-*
+* https://jira.rocket-internet.de/browse/TMLSD-*
+
+## Example
+Imagine to open issue "**TML-24974**" and the title is "**QC. The Price Section.2**", by clicking on the Extension icon you will get:
+
+
+Field                    | Value
+------------------------ | -----------------------------------
+**First commit message** | TML-24974 - QC. The Price Section.2
+**Branch name**          | TML-24974-qc.-the-price-section.2
+**Migration file**       | 20180425_1713_TML-24974.sql
+
+
---
+++ popup.js
@@ -0,0 +1,66 @@
+function leadingZero(aNumber) {
+    if (aNumber < 10) {
+        return '0' + '' + aNumber;
+    } else {
+        return aNumber;
+    }
+}
+
+chrome.runtime.onMessage.addListener(function(request, sender) {
+    if (request.action == "getSource") {
+        var page = jQuery(request.source);
+        var pageTitle = request.source.match(/<title>(.*?)<\/title>/)[1];
+        pageTitle = $('<div/>').html(pageTitle).text();
+
+        var firstCommitMessage = pageTitle.replace(" - JIRA", "")
+            .replace("[", "")
+            .replace("]", " -");
+
+        jQuery('#firstCommitMessage').val(firstCommitMessage);
+
+        var branchName = pageTitle.replace(" - JIRA", "")
+            .replace(/'/g, "")
+            .replace(/&/g, "and")
+            .replace(/\[/g, "")
+            .replace(/\]/g, "")
+            .replace(/:/g, "")
+            .replace(/"/g, "")
+            .replace(/ /g, "-")
+            .replace(/---/g, "-")
+            .replace(/\//g, "-")
+            .toLowerCase()
+            .replace("tmlsd", "TMLSD")
+            .replace("tml", "TML");
+
+        jQuery('#branchName').val(branchName);
+
+        var migrationFile = "#DATE#_#TIME#_#TML#.sql";
+
+        var now = new Date();
+        var currentMonth = leadingZero(now.getMonth() + 1);
+        var currentDay = leadingZero(now.getDate());
+        var currentHours = leadingZero(now.getHours());
+        var currentMinutes = leadingZero(now.getMinutes());
+
+        migrationFile = migrationFile.replace('#DATE#', now.getFullYear() + "" + currentMonth + "" + currentDay)
+            .replace('#TIME#', currentHours + "" + currentMinutes)
+            .replace('#TML#', pageTitle.match(/\[(.*?)\]/)[1]);
+
+        jQuery('#migrationFile').val(migrationFile);
+    }
+});
+
+function onWindowLoad() {
+
+    chrome.tabs.executeScript(null, {
+        file: "getPagesSource.js"
+    }, function() {
+        if (chrome.runtime.lastError) {
+            jQuery('#alert').html('<strong>Error: </strong>' + chrome.runtime.lastError.message)
+                .show();
+        }
+    });
+
+}
+
+window.onload = onWindowLoad;
